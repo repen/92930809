cffe6aaf51d3eb1fa190e397f9dff2487b855044
repen <9keepa@gmail.com>
2020-09-10 17:50:23 +0500

    Functions processing with help threadpoolexecutor

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import configparser
 import requests, time
-from multiprocessing import Process, SimpleQueue
 from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 from tools import log as lo
 from custom import functions
+from functools import partial
 
 
 
@@ -16,6 +17,7 @@ URL  = args['url']
 PATH = args['result_path']
 NAME = args['name']
 TIMEOUT = float( args['timeout'] )
+WORKER = int( args['worker'] )
 
 log = lo(NAME, "main.log")
 
@@ -61,17 +63,21 @@ def save( data ):
         f.write( string )
     log.info( "Save {} in {}".format( string, PATH ) )
 
+def call(f):
+    f()
 
 def _main():
     q = Queue()
     data = get_data()
-    processes = []
+    freeze = []
 
     for e, func in enumerate( functions , start=1):
-        processes.append( Thread(target=wrapper_run, args =  (q, func, data, e ) ) )
+        freeze.append( partial( wrapper_run, q, func, data, e ) )
 
-    [x.start() for x in processes]
-    [x.join() for x in processes]
+    with ThreadPoolExecutor(max_workers=WORKER) as executor:
+        for _ in executor.map(call, freeze):
+            pass
+
     results = []
     while True:
         items = q.get()
