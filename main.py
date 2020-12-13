e37c6128f0d5a42a0d663c5340d894ed5f15a305
repen <9keepa@gmail.com>
2020-12-13 21:15:20 +0500

    Get/Set Leverage

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import configparser, time, os, hmac, hashlib, requests
-from tools import log as lo
+from tools import log as lo, get_signature
 from datetime import datetime
 from urllib.parse import urlencode
+
+
+def get_timestamp():
+    # 1603616154651
+    # 1508396497000
+    return int(time.time() * 1000)
 
 config = configparser.ConfigParser()
 config.read('setting.conf')
@@ -13,14 +19,19 @@ LEVERAGE = int(args['leverage'])
 
 log = lo( NAME, "main.log")
 
-KEY = 'O0WIAYP8jc4tLZ03dc'
-SECRET = 'MsoezA2ruNnEGoaAQNHVXAQxL2GFLcHhLtbl'
+KEY = 'HL62GKHyIbXZraG4IB'
+SECRET = 'NUR8Z7c5EtEDiPBz05dr6KayMrvlW8NljDhD'
 BASE_URL = 'https://api.bybit.com' # production base url
 
-def get_timestamp():
-    # 1603616154651
-    # 1508396497000
-    return int(time.time() * 1000)
+def generate_sign():
+    secret = SECRET
+    params = {}
+    params['api_key'] = KEY
+    params['leverage'] = 100
+    params['symbol'] = "BTCUSD"
+    params['timestamp'] = str(get_timestamp())
+    return get_signature(secret, params)
+
 
 # curl https://api.bybit.com/user/leverage/save \
 # -H "Content-Type: application/json" \
@@ -28,44 +39,49 @@ def get_timestamp():
 
 
 def get_leverage():
-    # curl "https://api.bybit.com/user/leverage?api_key={api_key}&timestamp={timestamp}&sign={sign}"
-    headers = {"Content-Type": "application/json"}
+
+    params = {}
+    params['api_key'] = KEY
+    params['timestamp'] = str( get_timestamp() )
+
     url = BASE_URL + "/user/leverage"
-    payload = {
-        "api_key" : KEY,
-        "sign" : SECRET,
-        "timestamp" : get_timestamp(),
-    }
-    query_string = urlencode(payload, True)
-    response = requests.get( url + "?"+query_string )
-    print(url + "?"+query_string)
 
-    import pdb;pdb.set_trace()
-    pass
+    params["sign"] = get_signature(SECRET, params)
 
-def get_data():
+    query_string = urlencode(params, True)
+    response = requests.get( url + "?"+ query_string )
 
-    response01 = send_signed_request("GET",'/dapi/v1/positionRisk' , 
-        {"pair": "BTCUSD"}
-    )
-    
-    data_list = response01
-    data_list = list( filter( lambda x : x['symbol'] == "BTCUSD_PERP", data_list ) )
+    data = response.json()
+
+    # print(url + "?"+query_string)
 
     # import pdb;pdb.set_trace()
-    
-    data = data_list[0]
-    if int(data["leverage"]) == LEVERAGE:
-        log.info( "Miss. leverage == %s" % LEVERAGE ) 
-        return
+    log.info("Get leverage = %s" % data["result"]["BTCUSD"])
+    return data["result"]["BTCUSD"]["leverage"]
 
-    response02 = send_signed_request("POST",'/dapi/v1/leverage' , {
-            "symbol": "BTCUSD_PERP", 
-            "leverage" : int(LEVERAGE)
+# curl https://api.bybit.com/user/leverage/save \
+# -H "Content-Type: application/json" \
+# -d '{"api_key":"{api_key}","symbol":"BTCUSD","leverage":14,"timestamp":{timestamp},"sign":"{sign}"}'
 
-    })
+def set_leverage(number):
+    headers = {"Content-Type": "application/json"}
 
-    log.info( "leverage change! before %s | after %s" % ( data["leverage"], LEVERAGE ) )
+    params = {}
+    params['api_key'] = KEY
+    params['symbol'] = "BTCUSD"
+    params['timestamp'] = str(get_timestamp())
+    params['leverage'] = str(number)
+    params['sign'] = get_signature(SECRET, params)
+
+    url = BASE_URL + "/user/leverage/save"
+    response = requests.post( url, json=params, headers=headers )
+    log.info("Set leverage = %s" % response.text)
+
+
+def get_data():
+    leverage = get_leverage()
+    if leverage != LEVERAGE:
+        set_leverage( 53 )
 
 
 def _main():
@@ -78,7 +94,7 @@ def work():
     while True:
         try:
             main()
-            log.info("Sleep {}".format(TIMEOUT))
+            log.info("======= Sleep {} =======".format(TIMEOUT))
             time.sleep( TIMEOUT )
         except Exception as Er:
             # log.error("Error connect or work script {}. Wait 5 sec".format( Er ), exc_info=True)
@@ -87,10 +103,11 @@ def work():
 
 
 def test():
+    set_leverage(53)
     res = get_leverage()
     print(res)
 
 if __name__ == '__main__':
-    test()
+    # test()
     # main()
-    # work()
+    work()
